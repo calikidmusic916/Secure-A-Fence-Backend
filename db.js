eff728b64f0ba7d91b93181fa77ceaa6e1ce6828
unknown <your.email@example.com>
2026-09-12 12:10:07 -0700

    Fix native WebSocket missing error on Render by adding ws package

--- db.js
+++ db.js
@@ -11,8 +11,13 @@ const path = require('path');
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
 
+const ws = require('ws');
 const supabase = (createClient && SUPABASE_URL && SUPABASE_SERVICE_KEY)
-  ? createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY)
+  ? createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY, {
+      realtime: {
+        websocket: ws
+      }
+    })
   : null;
 
 if (!supabase) {
